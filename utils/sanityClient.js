06069726d0198e788249a80bd2e559e7d9573f5d
= <=>
2022-05-12 15:52:01 +0700

    fix env var

--- utils/sanityClient.js
+++ utils/sanityClient.js
@@ -1,7 +1,6 @@
 import sanityClient from '@sanity/client'
 import imageUrlBuilder from '@sanity/image-url'
 
-
 export const client = sanityClient({
     projectId: process.env.sanityProjectId,
     dataset: "production",
