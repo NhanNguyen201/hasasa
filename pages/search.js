bff6688862d0848c59c6d3f45a701571a835efe3
= <=>
2022-07-10 01:53:47 +0700

    fix search

--- pages/search.js
+++ pages/search.js
@@ -77,27 +77,13 @@ export async function getServerSideProps({query}) {
 
     let { _q }= query
     let decodedWord = decodeURI(_q)
-    const tidyAscent =(s) => {
-        var r=s.toLowerCase();
-        r = r.replace(new RegExp("\\s", 'g'),"");
-        r = r.replace(new RegExp("[àáâãäå]", 'g'),"a");
-        r = r.replace(new RegExp("æ", 'g'),"ae");
-        r = r.replace(new RegExp("ç", 'g'),"c");
-        r = r.replace(new RegExp("[èéêë]", 'g'),"e");
-        r = r.replace(new RegExp("[ìíîï]", 'g'),"i");
-        r = r.replace(new RegExp("ñ", 'g'),"n");                            
-        r = r.replace(new RegExp("[òóôõö]", 'g'),"o");
-        r = r.replace(new RegExp("œ", 'g'),"oe");
-        r = r.replace(new RegExp("[ùúûü]", 'g'),"u");
-        r = r.replace(new RegExp("[ýÿ]", 'g'),"y");
-        r = r.replace(new RegExp("\\W", 'g'),"");
-        return r;
+    const tidyAscent =(str) => {
+        str = str.toLowerCase();
+        return str.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
     };
     const posibleKeyWords = str => {
         let words = [];
-        words = words.concat(str.split(" "))
         words = words.concat(str.split(" ").map(w => tidyAscent(w)))        
-        words = words.concat(str)
 
         let reducedWords = words.reduce((acc, cur) => {
             if(!acc.find((word) => word == cur)) {
@@ -106,13 +92,14 @@ export async function getServerSideProps({query}) {
             } else return acc
        }, [])
 
-        let mapedWords = reducedWords.map(w => `'${w}*'`)
+        let mapedWords = reducedWords.map(w => `"${w}*"`)
         return mapedWords
     }
-    let productsquery  = `*[_type == "product" && title match [${posibleKeyWords(decodedWord)}]]{
+    let productsquery  = `*[_type == "product" && searchSlug.current match [${posibleKeyWords(decodedWord)}]]{
         ...,
         productPageRoute->
     }`
+
     const data = await client.fetch(productsquery)
 
     return {
