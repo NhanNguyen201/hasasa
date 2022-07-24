070df8646b7c84676873796bc25d931e3c425453
= <=>
2022-07-25 04:32:36 +0700

    dotenv add

--- next.config.js
+++ next.config.js
@@ -1,12 +1,13 @@
 /** @type {import('next').NextConfig} */
-
+const dotenv = require('dotenv')
+dotenv.config()
 
 
 module.exports =  {
   eslint: { ignoreDuringBuilds: true },
   // reactStrictMode: true,
   env: {
-    SANITY_PROJECT_ID: 'v03w5pfb',
-    SANITY_TOKEN: 'skfaMdsucxpCbQN6BBKocWGhIndnIi5BDgzT2aMqQA6j7ae17Tzs0ZeHqYeMuMJX0FCS58TRbmvBYJPpQ24U1VAvG2uYI39ihjShvDFp6xsqqfmrdQKNdaCFWeiRJBWScm9Xhkpmi5N086sjhsDTsS7yP5LwDJVsYO7FOWQ3dawz8FYrNpyd'
+    SANITY_PROJECT_ID: process.env.SANITY_PROJECT_ID,
+    SANITY_TOKEN: process.env.SANITY_TOKEN
   }
 }
